081c7a8446ebe54e32deb766b1e28d718110a4be
David Coombes <davidc@contentmastrer.com>
2019-01-29 12:38:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ response.write("<h1>Welcome to Contoso Fashions</h1>");
 response.write("<p>Style from the cloud.</p>");    
 response.write("<h2>Contoso Fashions</h2>");
 response.write("<p>Contoso Fashions is a world-leading online retailer of quality clothing.</p>");
-response.write("<table width="100%" class="text-table" ><tr>td>Clothing for men</td>td>Clothing for women</td><td>Footwear</td></tr><tr><td>Contoso Fashions has a great range of men's clothes.</td><td>Contoso Fashions has a great range of women's clothes.</td><td>Contoso Fashions has a great range of men's and women's shoes.</td></tr></table>" );
+response.write("<table><tr><td>Clothing for men</td>td>Clothing for women</td><td>Footwear</td></tr><tr><td>Contoso Fashions has a great range of men's clothes.</td><td>Contoso Fashions has a great range of women's clothes.</td><td>Contoso Fashions has a great range of men's and women's shoes.</td></tr></table>" );
 //Pi calculation
 var c=10000000;
 var Pi=0;
@@ -16,7 +16,7 @@ for (i=0;i<=c;i++)
 {
 Pi=Pi+(4/n)-(4/(n+2))
 n=n+4
-};
+}
 
 
 console.log("Pi calculated");
@@ -28,7 +28,7 @@ if (rnumber<2)
 console.error("Illegal operation", rnumber);
 response.write(rnumber);} //Illegal operation
 else
-{response.write("<br/>Random number is greater than 2, no error generated")};
+{response.write("<br/>Random number is greater than 2, no error generated");}
 
 response.end();
 
